ff0fea19c23e4698f42a52e8e54496fbdcdc7a1e
Paul Bailey <paul.m.bailey@gmail.com>
2020-10-17 13:51:58 -0500

    readme

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="trashpy",
-    version="1.0.0",
+    version="1.0.1",
     author="Paul Bailey",
     author_email="paul.m.bailey@gmail.com",
     description="Download your Google Drive Trash folder",
-    long_description="Download your Google Drive Trash folder",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pizzapanther/pydrive-trash-backup",
     packages=setuptools.find_packages(),
